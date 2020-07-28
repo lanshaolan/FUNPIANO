ae37eeb3ef8cf311017ce8de4c83185d4047c16b
lanshaolan <xanxuslan@gmail.com>
2020-07-28 20:12:20 +0800

    隨聲琴奏

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ console.log("我是JS");
 
 
 // 元件應用：分頁重複內容 - 導覽列、頁尾
-Vue.component("navbar", {
+Vue.component("navbar", { 
     template: `
     <nav>
-        <a href="./index.html">NORD</a>
+        <a href="./index.html">隨聲琴奏</a>
         <a href="./about.html">關於我們</a>
         <a href="./product.html">產品</a>
         <a href="./service.html">支援服務</a>
